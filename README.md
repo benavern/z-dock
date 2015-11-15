14510d06e0885adf12b13fa2f9b1a9885c16a916
Benjamin Caradeuc <benjamin.caradeuc@Benjamins-MacBook-Air.local>
2015-11-15 18:08:24 +0100

    POO version & minifications

---
+++ README.md
@@ -0,0 +1,5 @@
+# Z-Dock
+
+Simulate an OSX Dock with some vanilla Js and css3
+
+(Demo)[http://benavern.github.io/z-dock]
--- z-dock.js
+++ z-dock.js
@@ -1,46 +1,62 @@
 (function(){
 
-  var classdelay;
-
-  var hideDock= function (){
-    classdelay = setTimeout(function() {
-      document.getElementById("z-dock").removeAttribute('class');
-    }, 1000);
-  }
-
-  var showDock = function (){
-    clearTimeout(classdelay);
-    document.getElementById("z-dock").className = "shown";
-  }
-
-  var addClasses = function (e) {
-    var target = e.target;
-      if(target.getAttribute('src')) {
-        var li = target.parentNode.parentNode;
-        var prevLi = li.previousElementSibling;
-        var nextLi = li.nextElementSibling;
-        if(prevLi) {
-          prevLi.className = 'z-dock-prev';
-        }
-        if(nextLi) {
-          nextLi.className = 'z-dock-next';
-        }
-        target.addEventListener('mouseout', function() {
-          if(prevLi) {
-            prevLi.removeAttribute('class');
-          }
-          if(nextLi) {
-            nextLi.removeAttribute('class');
-          }
-        }, false);
+  this.ZDock = function() {
+    this.classdelay;
+    if(arguments[0]){
+      this._dockWrapper = arguments[0];
+      this._dock = this._dockWrapper.children[0];
+      this.start.call(this);
+    }
+    else{
+      throw "You must pass a DOM Object !";
     }
   }
 
+  this.ZDock.prototype.__showDock = function() {
+    var _this = this;
+    clearTimeout(_this._classdelay);
+    _this._dock.className = "shown";
+  };
 
-  document.getElementById("z-dock").addEventListener('mouseover', function(e) {
-    showDock()
-    addClasses(e);
-  });
-  document.getElementById("z-dock").addEventListener('mouseout', hideDock);
+  this.ZDock.prototype.__hideDock = function() {
+    var _this = this;
+    _this._classdelay = setTimeout(function() {
+      _this._dock.removeAttribute('class');
+    }, 1000);
+  };
+
+  this.ZDock.prototype.__addClasses = function(e) {
+    var target = e.target;
+    if(target.getAttribute('src')) {
+      var li = target.parentNode.parentNode;
+      var prevLi = li.previousElementSibling;
+      var nextLi = li.nextElementSibling;
+      if(prevLi) {
+        prevLi.className = 'z-dock-prev';
+      }
+      if(nextLi) {
+        nextLi.className = 'z-dock-next';
+      }
+      target.addEventListener('mouseout', function() {
+        if(prevLi) {
+          prevLi.removeAttribute('class');
+        }
+        if(nextLi) {
+          nextLi.removeAttribute('class');
+        }
+      });
+    }
+  };
+
+  this.ZDock.prototype.start = function() {
+    var _this = this;
+    _this._dock.addEventListener('mouseover', function(e) {
+      _this.__showDock.call(_this);
+      _this.__addClasses.call(_this, e);
+    });
+    _this._dock.addEventListener('mouseout', function() {
+      _this.__hideDock.call(_this);
+    });
+  }
 
 })();
